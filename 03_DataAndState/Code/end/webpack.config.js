9b6637f0fecf389f826f793de7c7f7d7e4624935
Rami Sayar <rami.sayar@gmail.com>
2016-05-06 09:47:44 -0400

    Added last part to module 3.

---
+++ 03_DataAndState/Code/end/webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname + '/app',
+  entry: {
+    javascript: './app.js',
+    html: './index.html',
+  },
+  output: { path: __dirname + '/dist', filename: 'app/app.js' },
+  module: {
+    loaders: [
+      {
+        test: /.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]",
+      }
+    ]
+  },
+};
