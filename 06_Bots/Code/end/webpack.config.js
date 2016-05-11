55efe3dbfc19b7282dd9ca87dd50073929288c64
Rami Sayar <rami.sayar@gmail.com>
2016-05-11 05:55:30 -0700

    Fixed code end.

--- 06_Bots/Code/end/webpack.config.js
+++ 06_Bots/Code/end/webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack');
 module.exports = {
   context: path.join(__dirname, 'app'),
   entry: {
-    javascript: './routes.js',
-    html: './index.html',
+    javascript: './app.js',
+    html: './index.html'
   },
   output: { 
     path: path.join(__dirname, 'dist'),
@@ -24,6 +24,18 @@ module.exports = {
       {
         test: /\.html$/,
         loader: "file?name=[name].[ext]",
+      },
+      {
+        test: /\.css$/, 
+        loader: "style-loader!css-loader" 
+      },
+      { 
+        test: /\.png$/, 
+        loader: "url-loader?limit=100000" 
+      },
+      { 
+        test: /\.jpg$/, 
+        loader: "file-loader" 
       }
     ]
   },
